5d2153a4d2b68ca516f9baa32b85bffe1af98e00
juhui0312 <89063842+juhui0312@users.noreply.github.com>
2022-12-21 13:47:14 -0800

    Applied black

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'mysite/templates')],
+        "DIRS": [os.path.join(BASE_DIR, "mysite/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -64,7 +64,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                
             ],
         },
     },
@@ -119,5 +118,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
